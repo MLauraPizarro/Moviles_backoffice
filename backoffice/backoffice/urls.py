aed9e7f985efeebedfad1fe62d22e73416144501
Maria Laura Pizarro <marilau63@gmail.com>
2018-03-27 21:39:03 -0600

    urls para el get

--- backoffice/backoffice/urls.py
+++ backoffice/backoffice/urls.py
@@ -8,6 +8,14 @@ from inicio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tag/', views.TagList.as_view()),
+    path('persona/', views.PersonaList.as_view()),
+    path('ingrediente/', views.IngredienteList.as_view()),
+    path('notificacion/', views.NotificacionList.as_view()),
+    path('comentario/', views.ComentarioList.as_view()),
+    path('seguidor/', views.SeguidoresXPersonaList.as_view()),
+    path('carrito/', views.CarritoList.as_view()),
+    path('receta/', views.RecetaList.as_view()),
+    path('favorito/', views.RecetaXPersonaList.as_view()),
     path('tag/(?P<pk>[0-9]+)/',views.TagDetail.as_view()),
 ]
 
