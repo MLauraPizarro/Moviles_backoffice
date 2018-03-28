0469253c0147633b288a4642f5433bb503d7bbeb
Maria Laura Pizarro <marilau63@gmail.com>
2018-03-28 16:20:12 -0600

    arreglo de permisos

--- backoffice/backoffice/settings.py
+++ backoffice/backoffice/settings.py
@@ -100,14 +100,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+'''
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        
     ]
 }
-
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
