d454ea3287928d7e8075ddf3f900f087c317700a
Anthony Alfaro <tonyfaros@gmail.com>
2018-04-14 01:28:42 -0600

    add duracion field

--- backoffice/inicio/models.py
+++ backoffice/inicio/models.py
@@ -89,7 +89,7 @@ class Receta(models.Model):
     notas = models.CharField(max_length = 100,blank=True)
     dificultad = models.CharField(max_length = 10)
     tiempo = models.TimeField(auto_now_add=True)
-    duracion = models.CharField(max_length = 10,blank=True,default="")
+    #duracion = models.CharField(max_length = 10,blank=True,default="")
     porciones = models.IntegerField()
     publico = models.BooleanField()
     costo = models.CharField(max_length = 50)
